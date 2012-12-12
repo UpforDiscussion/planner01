f14a4607e117b3fe390e61f1260b6db2879df952
Avinash Karnani <avi@avikarnani.com>
2012-12-12 15:12:52 -0500

    modifies database.yml and creates schema

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: questions
+#
+#  id              :integer          not null, primary key
+#  survey_id       :integer
+#  question_text   :text
+#  question_answer :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class Question < ActiveRecord::Base
   attr_accessible :question_answer, :question_text, :survey_id
 end
--- app/models/result.rb
+++ app/models/result.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: results
+#
+#  id         :integer          not null, primary key
+#  survey_id  :integer
+#  answer     :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Result < ActiveRecord::Base
   attr_accessible :answer, :survey_id
 end
